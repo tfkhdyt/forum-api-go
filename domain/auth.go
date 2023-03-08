983c78456d73e7891612315705b10538c4e41d10
tfkhdyt <tfkhdyt@proton.me>
2023-03-08 07:27:28 +0700

    feat(domain): create auth domain

---
+++ domain/auth.go
@@ -0,0 +1,42 @@
+package domain
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+// ==================
+
+type Auth struct {
+	gorm.Model
+	RefreshToken string `json:"refreshToken" gorm:"unique"`
+}
+
+type LoginLogoutDto struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
+type Credentials struct {
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+}
+
+type DeleteRefreshAuthDto struct {
+	RefreshToken string `json:"refreshToken" binding:"required,jwt"`
+}
+
+// ==================
+
+type AuthHandler interface {
+	Post(r *gin.Context)
+	Patch(r *gin.Context)
+	Delete(r *gin.Context)
+}
+
+type AuthService interface {
+	Login(loginDto LoginLogoutDto) (Credentials, error)
+	Logout(logoutDto LoginLogoutDto) error
+	Refresh(refreshDto DeleteRefreshAuthDto) (string, error)
+	Delete(deleteAuthDto DeleteRefreshAuthDto) error
+}
