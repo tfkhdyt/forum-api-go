fecb32e7c383cbfe508f6bff6674d6956283dae6
tfkhdyt <tfkhdyt@proton.me>
2023-03-10 21:50:21 +0700

    feat(domain): create token domain

---
+++ domain/token.go
@@ -0,0 +1,14 @@
+package domain
+
+import "github.com/golang-jwt/jwt/v5"
+
+type JwtClaims struct {
+	UserId uint `json:"userId"`
+	jwt.RegisteredClaims
+}
+
+type TokenService interface {
+	CreateAccessToken(userId uint) (string, error)
+	CreateRefreshToken(userId uint) (string, error)
+	DecodePayload(token string) (string, error)
+}
